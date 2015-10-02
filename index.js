ec96c19acdd154befbccb2519ed7c8643006a746
mejdi OURIR <m.ourir@ivsweb.com>
2015-10-02 15:48:58 +0200

    add ws transport server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.Server = require('./lib/server/');
-module.exports.Client = {};
-module.exports.Client.TCP = require('./lib/client/tcp/');
-
+module.exports.Server         = require('./lib/server/');
+module.exports.Client         = {};
+module.exports.Client.TCP     = require('./lib/client/tcp/');
+module.exports.cmdsDispatcher = require('./lib/cmdsDispatcher'); 
