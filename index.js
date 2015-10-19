ae355c62e5329582560a2d5638b3022c28ce41ce
François Leurent <fleurent@ivsweb.com>
2015-10-19 18:11:52 +0200

    WIP'

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 module.exports.Server         = require('./lib/server/');
 module.exports.Client         = {};
 module.exports.Client.TCP     = require('./lib/client/tcp/');
-module.exports.cmdsDispatcher = require('./lib/cmdsDispatcher'); 
