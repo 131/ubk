c42d2937156d13f878c1c7de921e5e607be228c0
Mejdi Ourir <m.ourir@ivsweb.com>
2017-05-31 11:23:21 +0200

    remove uclass from client side

--- client/ws/index.js
+++ client/ws/index.js
@@ -1,34 +1,26 @@
 "use strict";
 
 
-const Class   = require('uclass');
-const Options = require('uclass/options');
 const guid    = require('mout/random/guid');
 const once    = require('nyks/function/once');
 const merge   = require('mout/object/merge');
 const Client  = require('../');
 
 
-
-const WSClient = new Class({
-  Extends : Client,
-  Binds : ['receive', 'disconnect'],
-
-
-  url : '',
-  _socket : null,
-  client_key : null,
-  options : {
-    registration_parameters : {},
-  },
-
-  initialize : function(url , options) {
-    Client.prototype.initialize.call(this, options);
+class WSClient extends Client{
+  constructor(url , options){
+    options = Object.assign({
+      registration_parameters : {},
+    }, options);
+    super(options)
+    this.url = '';
+    this._socket = null;
+    this.client_key = null;
     this.url = url.replace('http','ws') ;
     this.client_key  = guid();
-  },
+  }
 
-  connect : function(chainConnect, chainDisconnect) {
+  connect(chainConnect, chainDisconnect) {
     var self = this ;
 
     this._onDisconnect = once(chainDisconnect || Function.prototype);
@@ -41,23 +33,22 @@ const WSClient = new Class({
       self._doConnect(chainConnect);
     };
 
-    this._socket.onmessage = this.receive;
-    this._socket.onclose   = this.disconnect;
-  },
+    this._socket.onmessage = this.receive.bind(this);
+    this._socket.onclose   = this.disconnect.bind(this);
+  }
 
-  write : function(data) {
+  write (data) {
     this._socket.send(JSON.stringify(data));
-  },
+  }
 
   // Received a message
-  receive : function(message) {
+  receive (message) {
     var data = JSON.parse(message.data) ;
     this._onMessage(data);
-  },
+  }
 
-
-  disconnect : function(error){
-    Client.prototype.disconnect.call(this);
+  disconnect(error){
+    super.disconnect();
 
     if(this._socket) {
       this._socket.close();
@@ -66,8 +57,8 @@ const WSClient = new Class({
 
     this._onDisconnect(error);
   }
+}
 
-});
 
 
 module.exports = WSClient;
